285c1125ad12a19ad06a2af701f31ac63da49afa
shade916 <privetabdul@gmail.com>
2021-03-16 23:06:55 +0300

    updated serializers

--- Core/serializers.py
+++ Core/serializers.py
@@ -1,7 +1,17 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
+from .models import Poll, Question
+
+
+class PollSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = Poll
         fields = '__all__'
+        # fields = ['name', 'start_date', 'finish_date', 'description', 'question_question_text']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['question_type', 'created']
+        # fields = '__all__'
