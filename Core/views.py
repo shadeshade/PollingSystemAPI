4b497e4734d096f370f59e8c7da75ec39eb867d6
shade916 <privetabdul@gmail.com>
2021-03-16 23:06:32 +0300

    updated views

--- Core/views.py
+++ Core/views.py
@@ -1,12 +1,31 @@
-from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import viewsets
+from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .models import Poll, Question
+from .serializers import PollSerializer, QuestionSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all().order_by('start_date')
+    serializer_class = PollSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        questions = instance.questions.all()
+        queryset = Poll.objects.all()
+        question_serializer = QuestionSerializer(questions)
+        return Response(question_serializer.data)
+
+
+# class QuestionViewSet(viewsets.ModelViewSet):
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
+#
+#     def retrieve(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance)
+#         questions = instance.questions.all()
+#         return Response(serializer.data)
 
